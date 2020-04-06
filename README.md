2d0c361a6b286b34a455fdbed06f1860f9b5647e
Felix Angelov <felangelov@gmail.com>
2020-04-06 17:43:21 -0500

    add min_coverage input

--- README.md
+++ README.md
@@ -8,10 +8,17 @@ A Github Action which helps enforce a minimum code coverage threshold.
 
 **Required** The path to the `lcov.info` file.
 
+### `min_coverage`
+
+**Required** The minimum coverage percentage allowed.
+
+**Default** 100
+
 ## Example usage
 
 ```yaml
 uses: ChicagoFlutter/lcov-cop@master
 with:
-  path: './coverage/lcov.info'
+  path: "./coverage/lcov.info"
+  min_coverage: 95
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -l
 
 LCOV_PATH=$1
+MINIMUM_COVERAGE=$2
 
-code_coverage=$(lcov --list ${LCOV_PATH} | sed -n "s/.*Total:|\(.*\)%.*/\1/p")
+CODE_COVERAGE=$(lcov --list ${LCOV_PATH} | sed -n "s/.*Total:|\(.*\)%.*/\1/p")
 
-echo "Code Coverage: ${code_coverage}%"
-if (( $(echo "$code_coverage < 100" | bc) )); then exit 1; fi
+echo "Minumum Coverage: ${MINIMUM_COVERAGE}$"
+echo "Code Coverage: ${CODE_COVERAGE}%"
+if (( $(echo "$CODE_COVERAGE < $2" | bc) )); then
+    exit 1;
+fi
