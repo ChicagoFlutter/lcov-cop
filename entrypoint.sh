a922b6603a37b669ce85f71d031926dee4d9be04
Felix Angelov <felangelov@gmail.com>
2020-04-06 17:50:30 -0500

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
-#!/bin/sh -l
+#!/bin/bash
+
+set -e
 
 LCOV_PATH=$1
 MINIMUM_COVERAGE=$2
 
 CODE_COVERAGE=$(lcov --list ${LCOV_PATH} | sed -n "s/.*Total:|\(.*\)%.*/\1/p")
 
-echo "Minumum Coverage: ${MINIMUM_COVERAGE}$"
+echo "Minumum Coverage: ${MINIMUM_COVERAGE}%"
 echo "Code Coverage: ${CODE_COVERAGE}%"
 if (( $(echo "$CODE_COVERAGE < $2" | bc) )); then
     exit 1;
